2a5be2a35732ed04495ec3d8a00a11aacb5ed252
Thomas Neos <isla@islas-mbp.home>
2021-05-01 10:43:11 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,18 +7,16 @@ weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  fetch(`http://localhost:3000/weather?address=${search.value}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-          messageTwo.textContent = "";
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${search.value}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        messageTwo.textContent = "";
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
   search.value = "";
 });
